e00d005a7ed851415277838910e1676e177f2004
Levin Rickert <levin.rickert@smacc.io>
2019-01-08 15:13:12 +0100

    Add babel-plugin-macros

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ module.exports = {
           plugins: [
             '@babel/plugin-proposal-class-properties',
             '@babel/plugin-proposal-object-rest-spread',
+            'babel-plugin-macros',
           ],
         },
       });
